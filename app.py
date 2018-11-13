9906d5b1d30d49b43bd11bb7e6bc2bbc3f4c648a
rudra <rudra@sanctusit.com>
2018-11-13 17:56:09 +0530

    first commit dreamland

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request
+#from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+@app.route('/', methods = ['GET','POST'])
+def index():
+    result=request.form.to_dict()
+    #print(result)
+    return render_template('landingpage.html')
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
