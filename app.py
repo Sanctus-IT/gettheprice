50f8eab9c54df9d688c589fbb13dbd6f78b6b7ff
rudra <rudra@sanctusit.com>
2018-11-14 11:12:56 +0530

    python v 3.X

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 @app.route('/', methods = ['GET','POST'])
 def index():
+
     result=request.form.to_dict()
-    #print(result)
+    print(result)
     return render_template('landingpage.html')
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
