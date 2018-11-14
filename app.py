6436232b20948fe4a8a0a9e8d65fb62b6a0d3e1f
rudra <rudra@sanctusit.com>
2018-11-14 18:24:55 +0530

    changes on reg form

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def index():
 
     result=request.form.to_dict()
-    print(result)
+    #print(result)
     return render_template('landingpage.html')
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
