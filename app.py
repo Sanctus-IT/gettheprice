765fc235bb6a10858a536736b7cdc3ca095ae896
rudra <rudra@sanctusit.com>
2018-11-22 12:12:07 +0530

    capture all dreams for datbase

--- app.py
+++ app.py
@@ -197,14 +197,59 @@ def verify():
 def post():
     try:
         result = request.form.to_dict()
-        #print(result)
+        print(result)
         if result=={}:
             return render_template('dream_post.html')
         elif len(result['dream_type'])>1:
             dream=result['dream']
+            import random
+            for x in range(1):
+                id1=random.randint(1, 100000)
+            db = psycopg2.connect(
+                database="dcore2hl3fm13v",
+                user="pnevkxlqdlmdif",
+                password="4d4a6fea5afacaab6d2e7372233725045c0b183e96925dec212ddf0ac468cdc1",
+                host="ec2-174-129-192-200.compute-1.amazonaws.com"
+            )
+            # db = psycopg2.connect(
+            #     database="Dreamland",
+            #     user="postgres",
+            #     password="1234",
+            #     host="localhost"
+            # )
+            cur = db.cursor()
+            cur.execute(
+                "INSERT INTO user_dreams (id,user_name,date_post,dream) VALUES ('{}',0,0,'{}')".format(
+                    id1,result['dream']))
+            # dreams_user=cur.execute("SELECT dream From user_dreams")
+            print(id1)
+            db.commit()
+            print(result['dream'])
+            db.close()
+            db = psycopg2.connect(
+                database="dcore2hl3fm13v",
+                user="pnevkxlqdlmdif",
+                password="4d4a6fea5afacaab6d2e7372233725045c0b183e96925dec212ddf0ac468cdc1",
+                host="ec2-174-129-192-200.compute-1.amazonaws.com"
+            )
+            # db = psycopg2.connect(
+            #     database="Dreamland",
+            #     user="postgres",
+            #     password="1234",
+            #     host="localhost"
+            # )
+            cur = db.cursor()
+            cur.execute("SELECT dream From user_dreams")
+            dream_user1 = cur.fetchall()
+            #print(tuple())
+            db.commit()
+            db.close()
+            dream_user=[]
+            for i in dream_user1:
+                dream_user.append(str(i))
             post_type=result['category']
             msg="Thanks for feeling  out your dream"
-            return render_template('dream_post.html',dream=dream,msg=msg)
+            return render_template('dream_post.html',dream=dream,msg=msg,dream_user=dream_user)
     except:
         pass
 if __name__ == '__main__':
